fe725427e9ffb775823cb578a21d7ed353fb4853
Yantha <bryanananta52@gmail.com>
2025-06-23 01:20:29 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        https: true,
-    },
+    base: '/', // biar path-nya relatif
     plugins: [
         laravel({
             input: [
                 'resources/js/app.js',
+                'resources/css/homepage.css',
             ],
             refresh: true,
         }),
     ],
-    base: '', 
 });
