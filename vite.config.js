cdb85a0bd2f6b856df0ad6c254edcb7d29e19651
Yantha <bryanananta52@gmail.com>
2025-06-23 01:15:43 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        https: true,
+    },
     plugins: [
         laravel({
-            input: ['resources/js/app.js',],
+            input: [
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
+    base: '', 
 });
